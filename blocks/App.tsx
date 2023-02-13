104694631269d914928c0300e09620ed55f022d4
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2023-02-13 15:09:30 +0100

    chore: add metadata debug infos
    [skip ci]

--- blocks/App.tsx
+++ blocks/App.tsx
@@ -3,7 +3,13 @@ import { type FileBlockProps, getLanguageFromFilename } from '@githubnext/blocks
 import { ErrorBoundary } from './components/ErrorBoundary'
 import { Playground } from './components/Playground'
 
-export default function App({ content, context }: FileBlockProps) {
+export default function App({ content, context, metadata, options }: AppProps) {
+  // TODO(HiDeoo)
+  // eslint-disable-next-line no-console
+  console.log('🚨 [App.tsx:7] metadata:', metadata)
+  // eslint-disable-next-line no-console
+  console.log('🚨 [App.tsx:7] options:', options)
+
   const extension = (context.path ? getLanguageFromFilename(context.path) : 'N/A') === 'JavaScript' ? 'js' : 'ts'
 
   return (
@@ -11,4 +17,10 @@ export default function App({ content, context }: FileBlockProps) {
       <Playground content={content} extension={extension} />
     </ErrorBoundary>
   )
+}
+
+interface AppProps extends FileBlockProps {
+  options?: {
+    version?: string
+  }
 }
--- examples/README.md
+++ examples/README.md
@@ -6,4 +6,9 @@
 
 This is a simple example of the TS Playground GitHub Block embedded in Markwdown.
 
-<BlockComponent block={{"owner":"hideoo","repo":"ts-playground-block","id":"ts-playground-block","type":"file"}} context={{"owner":"hideoo","repo":"ts-playground-block","path":"examples/types.ts","sha":"HEAD","file":"examples/README.md"}} />
+<BlockComponent
+block={{"owner":"hideoo","repo":"ts-playground-block","id":"ts-playground-block","type":"file"}}
+context={{"owner":"hideoo","repo":"ts-playground-block","path":"examples/types.ts","sha":"HEAD","file":"examples/README.md"}}
+metadata={{"version":"custom_metadata"}}
+options={{"version":"custom_metadata"}}
+/>
